46a867c72344d3a8978092bb2bce1624368de369
FranciscoCamurca <33267728+FranciscoCamurca@users.noreply.github.com>
2021-10-05 11:47:11 -0300

    inclusao da biblioteca CORS

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
@@ -11,4 +13,4 @@ app.use(morgan('dev'));
 require('./app/controllers/index')(app);
 
 app.listen(36300);
-console.log("servidor ok")
+console.log('easy-tracking started');
